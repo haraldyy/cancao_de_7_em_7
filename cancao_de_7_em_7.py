6df46ce017083781de5c30e9aad7731805c5295f
haraldyy <69537970+haraldyy@users.noreply.github.com>
2020-09-13 13:25:42 -0300

    Add files via upload

---
+++ cancao_de_7_em_7.py
@@ -0,0 +1,9 @@
+def cancao ():
+    for i in range(99, 251,7):
+        print(f'{i} bugs no software, pegue sete deles e conserte...')
+        print('Tecle "Ctrl+F5"')
+    print('Vamos fazer mais um café!')
+def main():
+    cancao()
+if __name__=='__main__':
+    main()
